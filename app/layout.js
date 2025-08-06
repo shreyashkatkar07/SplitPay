9aa065c086270297a775eb1388b8cc52648df45a
Shreyash Katkar <shreyashkatkar04@gmail.com>
2025-08-06 08:27:46 +0530

    first commit

---
+++ app/layout.js
@@ -0,0 +1,37 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+
+const inter = Inter({subsets: ["latin"],});
+
+export const metadata = {
+  title: "Splitr",
+  description: "Your go to app for spliting expenses",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="\logos\logo-s.png" />
+      </head>
+      <body
+        className={`${inter.className}`}
+      >
+        <ClerkProvider>
+        <ConvexClientProvider>
+        <Header />
+
+        <main className="min-h-screen">
+          {children}
+          <Toaster richColors />
+        </main>
+        </ConvexClientProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
